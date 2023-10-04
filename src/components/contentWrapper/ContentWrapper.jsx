f53f26ff33a23742ca61482a2a46151b288de64e
Shahanawaz <shahanawazkhan579@gmail.com>
2023-10-04 09:24:31 +0530

    Completed HeroBanner & Footer

---
+++ src/components/contentWrapper/ContentWrapper.jsx
@@ -0,0 +1,12 @@
+import "./style.scss";
+import PropTypes from "prop-types";
+
+const ContentWrapper = ({ children }) => {
+  return <div className="contentWrapper">{children}</div>;
+};
+
+ContentWrapper.propTypes = {
+  children: PropTypes.node,
+};
+
+export default ContentWrapper;
---
+++ src/components/lazyLoadingImage/Img.jsx
@@ -0,0 +1,14 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import PropTypes from "prop-types";
+
+const Img = ({ src, className }) => {
+  return <LazyLoadImage className={className || ""} alt="BackdropImage" effect="blur" src={src} />;
+};
+
+Img.propTypes = {
+  src: PropTypes.string,
+  className: PropTypes.string,
+};
+
+export default Img;
