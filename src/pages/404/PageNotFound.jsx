cb92ec7f14f02ff7cdb2df0ce8bba01dc447dc16
Shahanawaz <shahanawazkhan579@gmail.com>
2023-10-05 16:54:29 +0530

    completed 404 page

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,7 +1,19 @@
 import "./style.scss";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="pageNotFound">
+      <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+        <Link to="/">
+          <button type="button">Homepage</button>
+        </Link>
+      </ContentWrapper>
+    </div>
+  );
 };
 
 export default PageNotFound;
