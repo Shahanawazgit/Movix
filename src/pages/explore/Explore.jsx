ca64c5ac17493c28045bc822b6b198ddd5922c5f
Shahanawaz <shahanawazkhan579@gmail.com>
2023-10-05 16:55:51 +0530

    Fixed noResults bug

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Select from "react-select";
 import "./style.scss";
+import noResults from "../../assets/no-results.png";
 import useFetch from "../../hooks/useFetch";
 import { fetchDataFromApi } from "../../utils/api";
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
@@ -135,7 +136,10 @@ const Explore = () => {
                 })}
               </InfiniteScroll>
             ) : (
-              <span className="resultNotFound">Sorry, Results not found!</span>
+              <div className="resultNotFound">
+                <span>Sorry, Results not found!</span>
+                <img src={noResults} alt="noResults" />
+              </div>
             )}
           </>
         )}
